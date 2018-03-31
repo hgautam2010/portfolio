5d52210da1234f68ad7e1f1bb9987cdd31b1154c
Himanshu <himanshu@Himanshus-MacBook-Air.local>
2018-03-31 23:33:07 +0530

    Update
    transition time,
    Beautify

--- particle.js
+++ particle.js
@@ -6,7 +6,7 @@ function particle() {
   this.g = random(255);
   this.b = random(255);
   this.lineWith = (p) => {
-    stroke(this.r,this.g,this.b, map(1300 - dist, 0, 1300, -1500, 255));
+    stroke(this.r, this.g, this.b, map(1300 - dist, 0, 1300, -1500, 255));
     strokeWeight(2);
     line(this.loc.x, this.loc.y, p.x, p.y);
   }
@@ -20,23 +20,19 @@ function particle() {
     this.acc.mult(0);
   }
   this.check = () => {
-    if(this.loc.x < 0)
-    {
+    if (this.loc.x < 0) {
       this.loc.x = 0;
       this.vel.x *= -1;
     }
-    if(this.loc.x > width)
-    {
+    if (this.loc.x > width) {
       this.loc.x = width;
       this.vel.x *= -1;
     }
-    if(this.loc.y < 0)
-    {
+    if (this.loc.y < 0) {
       this.loc.y = 0;
       this.vel.y *= -1;
     }
-    if(this.loc.y > height)
-    {
+    if (this.loc.y > height) {
       this.loc.y = height;
       this.vel.y *= -1;
     }
@@ -44,7 +40,7 @@ function particle() {
   this.display = () => {
     this.move();
     noStroke();
-    fill(this.r,this.g,this.b);
+    fill(this.r, this.g, this.b);
     ellipseMode(CENTER);
     ellipse(this.loc.x, this.loc.y, 7, 7);
   }
